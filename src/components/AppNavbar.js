979c0d38cca245d0c421b531350d3a77145d8589
vbacic <valentina.bacic2@gmail.com>
2021-05-06 18:26:52 +0200

    dodavanje navigacije

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+export default class AppNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isOpen: false};
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return <Navbar color="dark" dark expand="md">
+      <NavbarBrand tag={Link} to="/">HGSS dojavni centar</NavbarBrand>
+      <NavbarToggler onClick={this.toggle}/>
+      <Collapse isOpen={this.state.isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink
+              href="http://www.hgss.hr/">Službena web stranica</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>;
+  }
+}
